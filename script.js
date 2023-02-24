aeb55339e46f020b17c1da57b37fdcfbc6e810fd
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-02-24 20:58:35 +0400

    Add password pattern check

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const form = document.forms['example'];
 const email = form.elements['email'];
-const emailError = document.querySelector('#email ~ .invalid');
+const emailError = document.querySelector('.email .invalid');
 const password = form.elements['pass'];
-const passwordError = document.querySelector('#pass ~ .invalid');
+const passwordError = document.querySelector('.pass .invalid');
 const confirmPass = form.elements['confirm'];
-const confirmError = document.querySelector('#confirm ~ .invalid');
+const confirmError = document.querySelector('.confirm .invalid');
 
-function validateEmail(input) {
-  return;
+function validateEmail() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = 'Email required';
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = 'Please enter a valid email';
+  } else {
+    emailError.textContent = '';
+  }
 }
 
-function validatePassword(input) {
-  return;
+function validatePassword() {
+  if (password.validity.valueMissing) {
+    passwordError.textContent = 'Password required';
+  } else if (password.validity.tooShort) {
+    passwordError.textContent = 'Minimum length 10 characters';
+  } else if (password.validity.tooLong) {
+    passwordError.textContent = 'Maximum length 18 characters';
+  } else if (password.validity.patternMismatch) {
+    passwordError.textContent =
+      'The password must contain at least one letter, one number and one special character';
+  } else {
+    passwordError.textContent = '';
+  }
 }
 
-function validateConfirm(input) {
-  return;
+function validateConfirm() {
+  if (confirmPass.value != password.value) {
+    confirmError.textContent = 'Password mismatch';
+  } else {
+    confirmError.textContent = '';
+  }
 }
+
+email.addEventListener('focusout', () => validateEmail());
+password.addEventListener('input', () => validatePassword());
+confirmPass.addEventListener('input', () => validateConfirm());
 
 function validateForm() {
   validateEmail(email);
@@ -30,34 +55,4 @@ function validateForm() {
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   validateForm();
-});
-
-email.addEventListener('focusout', () => {
-  if (email.validity.valueMissing) {
-    emailError.textContent = 'Email required';
-  } else if (email.validity.typeMismatch) {
-    emailError.textContent = 'Please enter a valid email';
-  } else {
-    emailError.textContent = '';
-  }
-});
-
-password.addEventListener('focusout', () => {
-  if (password.validity.valueMissing) {
-    passwordError.textContent = 'Password required';
-  } else if (password.validity.tooShort) {
-    passwordError.textContent = 'Minimum length 10 characters';
-  } else if (password.validity.tooLong) {
-    passwordError.textContent = 'Maximum length 18 characters';
-  } else {
-    passwordError.textContent = '';
-  }
-});
-
-confirmPass.addEventListener('input', () => {
-  if (confirmPass.value != password.value) {
-    confirmError.textContent = 'Password mismatch';
-  } else {
-    confirmError.textContent = '';
-  }
 });
